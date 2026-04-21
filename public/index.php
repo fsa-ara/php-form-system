49aa62fde7dad70fcf7233921125d986bfcb92a7
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-04-21 16:29:42 +0200

    chore: add index entrypoint

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+
+use Core\Autoloader;
+use Src\FormHandler;
+
+require_once __DIR__ . "/../core/Autoloader.php";
+
+Autoloader::init();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP FORM SYSTEM</title>
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+
+<body class="bg-light">
+    <?php
+    $form = new FormHandler();
+    $form->render();
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
