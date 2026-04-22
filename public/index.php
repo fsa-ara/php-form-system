a8802acfc34a05ff541f9879a670e66d617d4822
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-04-22 11:17:36 +0200

    feat(chore): add session

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ require_once __DIR__ . "/../core/Autoloader.php";
 
 Autoloader::init();
 
+session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +22,7 @@ Autoloader::init();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
 </head>
 
-<body class="bg-light">
+<body class="bg-light position-relative">
     <?php
     $form = new FormHandler();
     $form->render();
