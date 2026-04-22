a70409f8cb942c5831684e3c6c50450b7611f078
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-04-22 13:18:14 +0200

    feat(views): add header

---
+++ views/header.php
@@ -0,0 +1,10 @@
+<header class="position-fixed w-100 z-1 bg-primary">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link text-white" href="/">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="form">Contact form</a>
+        </li>
+    </ul>
+</header>
